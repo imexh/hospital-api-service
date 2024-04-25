ce51abe7965acf6473b640211440d99ceec7001b
imexh <chandikaranawella@gmail.com>
2024-04-25 16:39:44 +0530

    first commit

---
+++ src/main/java/com/application/applicationapiservice/ApplicationApiServiceApplication.java
@@ -0,0 +1,13 @@
+package com.application.applicationapiservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationApiServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationApiServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/application/applicationapiservice/ApplicationApiServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.application.applicationapiservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationApiServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
